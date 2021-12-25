babec102477db188cc8cac4de41b32c9318640f9
riyenas0925 <riyenas0925@gmail.com>
2021-12-25 18:57:00 +0900

    init : initialize spring boot project

---
+++ src/main/java/dev/riyenas/growing/GrowingApplication.java
@@ -0,0 +1,13 @@
+package dev.riyenas.growing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/riyenas/growing/GrowingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.riyenas.growing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
